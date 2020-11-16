4cd5f5d5a4e8361b2ca8e3abf5e1ecd38b2780c8
Thiago A. Marcelino <thiagodistend@hotmail.com>
2020-11-16 14:52:20 -0300

    Create and list Transaction

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,23 +1,36 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
 transactionRouter.post('/', (request, response) => {
+  const { title, value, type } = request.body;
   try {
-    // TODO
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const trasaction = createTransaction.execute({ title, type, value });
+
+    return response.json(trasaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
